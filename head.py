d0b623062e8dfa43f9a977ace90f9f074bff050e
rasdani <73563550+rasdani@users.noreply.github.com>
2023-11-09 19:49:06 +0100

    change to Assistant API

--- head.py
+++ head.py
@@ -1,144 +1,182 @@
-import json
-import requests
-import os
-import openai
+from openai import OpenAI
+import instructor
+from enum import Enum
+from pydantic import BaseModel, validator
+from urllib.parse import urlparse
+from typing import Union
 from dotenv import load_dotenv
-from tenacity import retry, wait_random_exponential, stop_after_attempt
-from playwright.sync_api import sync_playwright, Playwright
-from drive_browser import WebDriver
-
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-# GPT_MODEL = "gpt-3.5-turbo-0613"
-GPT_MODEL = "gpt-3.5-turbo"
-# GPT_MODEL = "gpt-4"
+client = instructor.patch(OpenAI())
+# MODEL = "gpt-4-1106-preview"
+# MODEL = "gpt-4"
+MODEL = "gpt-3.5-turbo-1106"
+
+class FunctionName(Enum):
+    VISIT_WEBSITE = 'visit_website'
+    EXECUTE_BROWSER_ACTION = 'execute_browser_action'
+
+class VisitWebsiteArgs(BaseModel):
+    url: str
+
+    @validator('url')
+    def check_url(cls, v):
+        parsed = urlparse(v)
+        if not all([parsed.scheme, parsed.netloc]):
+            raise ValueError('Invalid URL')
+        return v
 
 
-# @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
-# def chat_completion_request(messages, functions=None, function_call=None, model=GPT_MODEL):
-#     print(f"{model=}")
-#     headers = {
-#         "Content-Type": "application/json",
-#         "Authorization": "Bearer " + openai.api_key,
-#     }
-#     # json_data = {"model": model, "messages": messages}
-#     json_data = {"model": model, "temperature": 0, "messages": messages}
-#     if functions is not None:
-#         json_data.update({"functions": functions})
-#     if function_call is not None:
-#         json_data.update({"function_call": function_call})
-#     try:
-#         response = requests.post(
-#             "https://api.openai.com/v1/chat/completions",
-#             headers=headers,
-#             json=json_data,
-#         )
-#         print(f"{response.status_code=}")
-#         return response
-#     except Exception as e:
-#         print("Unable to generate ChatCompletion response")
-#         print(f"Exception: {e}")
-#         return e
+class BrowserAction(Enum):
+    GO_BACK = 'go_back'
+    GO_FORWARD = 'go_forward'
+    NEW_TAB = 'new_tab'
 
-def chat_completion_request(messages, functions=None, function_call="auto", model=GPT_MODEL):
-    print(f"{model=}")
-    try:
-        completion = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        functions=functions,
-        function_call=function_call,
-        temperature=0,
-        )
-        return completion
-    except Exception as e:
-        print("Unable to generate ChatCompletion response")
-        print(f"Exception: {e}")
-        return e
+class ExecuteBrowserActionArgs(BaseModel):
+    action: BrowserAction
 
-functions = [
+class FunctionCall(BaseModel):
+    function_name: FunctionName
+    arguments: Union[VisitWebsiteArgs, ExecuteBrowserActionArgs, None]
+
+
+def visit_website(driver, url):
+    driver.navigate_to(url)
+    return driver
+
+def execute_browser_action(driver, action: BrowserAction):
+    page = driver.page
+    if action == BrowserAction.GO_BACK:
+        page.go_back()
+    elif action == BrowserAction.GO_FORWARD:
+        page.go_forward()
+    elif action == BrowserAction.NEW_TAB:
+        new_page = page.context.new_page()
+        driver.page = new_page
+    else:
+        raise ValueError(f"Unknown action: {action.action}")
+    return driver
+
+def execute_function_call(driver, function_call: FunctionCall):
+    if function_call.function_name == FunctionName.VISIT_WEBSITE:
+        url = function_call.arguments.url
+        driver = visit_website(driver, url)
+    elif function_call.function_name == FunctionName.EXECUTE_BROWSER_ACTION:
+        action = function_call.arguments.action
+        driver = execute_browser_action(driver, action)
+    else:
+        raise ValueError(f"Error: function {function_call.function_name} does not exist")
+    return driver
+
+tools = [
     {
-        "name": "visit_website",
-        "description": "Use this function to visit a website requested by the user.",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "url": {
-                    "type": "string",
-                    "description": "The URL to enter into the browser",
+        "type": "function",
+        "function": {
+            "name": "visit_website",
+            "description": "Use this function to visit a website requested by the user.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "url": {
+                        "type": "string",
+                        "description": "The URL to enter into the browser",
+                    },
                 },
+                "required": ["url"],
             },
-            "required": ["url"],
         },
     },
     {
-        "name": "execute_browser_action",
-        # "description": "Execute a given action in the browser",
-        "description": "Use this function to go back, go forward, or open a new tab",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "action": {
-                    "type": "string",
-                    "description": "The action to be performed in the browser. Can be 'go_back', 'go_forward', or 'new_tab'.",
+        "type": "function",
+        "function": {
+            "name": "execute_browser_action",
+            "description": "Use this function to go back, go forward, or open a new tab",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "action": {
+                        "type": "string",
+                        "description": "The action to be performed in the browser. Can be 'go_back', 'go_forward', or 'new_tab'.",
+                    },
                 },
+                "required": ["action"],
             },
-            "required": ["action"],
         },
     }
 ]
 
-def visit_website(url):
-    with sync_playwright() as playwright:
-        driver = WebDriver(playwright)
-        driver.start_browser(headless=False)
-        driver.navigate_to(url)
-    return "Success"
+# Create Assistant
+assistant = client.beta.assistants.create(
+    name="Web Browser Assistant",
+    instructions="You are the operator of a web browser. Translate user requests into calls of the provided browser functions.",
+    tools=tools,
+    model=MODEL,
+)
 
-def execute_function_call(function_call):
-    if function_call["name"] == "visit_website":
-        url = json.loads(function_call["arguments"])["url"]
-        results = visit_website(url)
-    else:
-        results = f"Error: function {function_call['name']} does not exist"
-    return results
+# Create Thread
+thread = client.beta.threads.create()
 
+# Add a message to the thread
+user_message = "Visit google"
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content=user_message,
+)
+thread_messages = client.beta.threads.messages.list(thread_id=thread.id)
+# print(f"{thread.data=}")
 
+# Run the Assistant
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+#   instructions="Please address the user as Jane Doe. The user has a premium account."
+)
 
-messages = []
-# system_message = "Navigate a web browser with the provided functions."
-# system_message = "You are the operator of a web browser. Your objective is to understand user requests and translate them into proper calls of the provided browser functions."
-system_message = "Use function_call to translate user requests into proper calls of the provided browser functions."
-# system_message = """
-# You are a helpful assistant. 
-# Respond to the following prompt by using function_call and then summarize actions. 
-# Ask for clarification if a user request is ambiguous.
-# """
-# system_message = """
-# You are a the operator of web browser. 
-# Respond to the following prompt by using function_call to call the provided functions with proper arguments. 
-# Don not talk, just execute the function calls. You are an operator, not a conversation partner.
-# """
-messages.append({"role": "system", "content": system_message})
-# messages.append({"role": "user", "content": "Hi, who are the top 5 artists by number of tracks?"})
+# Retrive run and check its status
+run = client.beta.threads.runs.retrieve(
+  thread_id=thread.id,
+  run_id=run.id,
+)
+print(f"{run.status=}")
 
-if __name__ == "__main__":
-    while True:
-        user_input = input("Enter your message: ")
-        if user_input.lower() == "quit":
-            break
-        messages.append({"role": "user", "content": user_input})
-        chat_response = chat_completion_request(
-            messages,
-            functions=functions,
-        )
-        print(f"{chat_response=}")
-        print(f"{chat_response.text=}")
-        assistant_message = chat_response.json()["choices"][0]["message"]
-        if "function_call" in assistant_message:
-            function_call = assistant_message["function_call"]
-            execute_function_call(function_call)  # Assuming this function is set up to handle the function call object
-        else:
-            messages.append(assistant_message)
-            print("Assistant's response: ", assistant_message["content"])
+# Retrieve Assistant's response
+messages = client.beta.threads.messages.list(
+  thread_id=thread.id
+)
+print(f"{messages=}")
+
+# Optionally inspect the run steps
+run_steps = client.beta.threads.runs.steps.list(
+    thread_id=thread.id,
+    run_id=run.id
+)
+print(f"{run_steps=}")
+
+# Retriece run to get function call ids
+# Retrive run and check its status
+run = client.beta.threads.runs.retrieve(
+  thread_id=thread.id,
+  run_id=run.id,
+)
+tool_calls = run.required_action.submit_tool_outputs.tool_calls
+print(f"{tool_calls=}")
+# call_ids = [call.tool_call_id for call in tool_calls]
+
+### excute function
+
+# Submit function call outputs
+# run = client.beta.threads.runs.submit_tool_outputs(
+#   thread_id=thread.id,
+#   run_id=run.id,
+#   tool_outputs=[
+#       {
+#         "tool_call_id": call_ids[0],
+#         "output": "22C",
+#       },
+#       {
+#         "tool_call_id": call_ids[1],
+#         "output": "LA",
+#       },
+#     ]
+# )
---
+++ pix2struct.py
@@ -0,0 +1,16 @@
+import requests
+from PIL import Image
+from transformers import Pix2StructForConditionalGeneration, Pix2StructProcessor
+
+url = "https://www.ilankelman.org/stopsigns/australia.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+model_id = 'gitlost-murali/pix2struct-refexp-base'
+model = Pix2StructForConditionalGeneration.from_pretrained(model_id)
+processor = Pix2StructProcessor.from_pretrained(model_id)
+
+text = "What is in the picture?"
+inputs = processor(images=image, text=text, return_tensors="pt")
+
+predictions = model.generate(**inputs)
+print(processor.decode(predictions[0], skip_special_tokens=True))
