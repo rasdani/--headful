556c5648127057f019cd93d19de52d0555bac90d
rasdani <73563550+rasdani@users.noreply.github.com>
2023-11-10 20:59:22 +0100

    refactor eyes

--- eyes.py
+++ eyes.py
@@ -13,40 +13,53 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 # Path to your image
-image_path = "screenshot_after.png"
 # message = "The little Github logo in this screenshot is annotated with a small yellow lable. What letter is on the label?"
-message = "The 'Sign in' button in this screenshot is annotated with a small yellow label. What letter is on the label?"
 
-# Getting the base64 string
-base64_image = encode_image(image_path)
+def see(image_path, user_request):
+    # breakpoint()
+    base64_image = encode_image(image_path)
+    message = """This is a screenshot of a web page. The clickable elements of the page are annotated with small yellow labels containing a code of one or two letters. \
+        Now follows a user request. Interpret which element the user wants to click on and return the corresponding letter code. \
+        Respond by citing the letter code only!
+    USER REQUEST: {user_request}"""
+    message = message.format(user_request=user_request)
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
-}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
 
-payload = {
-    "model": "gpt-4-vision-preview",
-    "messages": [
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": message
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+            {
+                "type": "text",
+                "text": message
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                # "url": f"data:image/jpeg;base64,{base64_image}"
+                "url": f"data:image/png;base64,{base64_image}"
+                }
             }
-          }
-        ]
-      }
-    ],
-    "max_tokens": 300
-}
+            ]
+        }
+        ],
+        "max_tokens": 300
+    }
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response_json = response.json()
 
-print(response.json())
+    print(response_json)
+    # return response['choices'][0]['message']['content']
+
+if __name__ == "__main__":
+    image_path = "screenshot_after.png"
+    while True:
+        user_request = input("User request: ")
+        see(image_path, user_request)
