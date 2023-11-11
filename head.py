83aaeb21602e0f9147578b6c4571e76453beb891
rasdani <73563550+rasdani@users.noreply.github.com>
2023-11-11 16:23:07 +0100

    instructor does not yet support Assistant API

--- head.py
+++ head.py
@@ -1,3 +1,4 @@
+### instructor has way better undocumented features now
 from time import sleep
 from openai import OpenAI
 import instructor
@@ -9,9 +10,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 client = instructor.patch(OpenAI())
-# MODEL = "gpt-4-1106-preview"
+GPT_MODEL = "gpt-4-1106-preview"
 # MODEL = "gpt-4"
-MODEL = "gpt-3.5-turbo-1106"
+# GPT_MODEL = "gpt-3.5-turbo-1106"
 # MODEL = "gpt-3.5-turbo"
 
 
@@ -79,7 +80,7 @@ def execute_function_call(driver, function_call: FunctionCall):
     return driver
 
 
-tools = [
+functions = [
     {
         "type": "function",
         "function": {
@@ -116,121 +117,18 @@ tools = [
     },
 ]
 
-
-def create_assistant(**kwargs):
-    assistant = client.beta.assistants.create(**kwargs)
-    return assistant
-
-
-def create_thread():
-    thread = client.beta.threads.create()
-    return thread
-
-
-def add_message_to_thread(thread, message):
-    message_obj = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=message,
-    )
-    return message_obj
-
-
-def list_messages(thread):
-    thread_messages = client.beta.threads.messages.list(thread_id=thread.id)
-    return thread_messages
-
-
-def create_assistant_run(thread, assistant):
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id,
-    )
-    return run
-
-
-def get_run(thread, run):
-    run = client.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id,
-    )
-    return run
-
-
-def update_run(thread, run):
-    run = client.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id,
-    )
-    return run
-
-
-def get_run_steps(thread, run):
-    run_steps = client.beta.threads.runs.steps.list(thread_id=thread.id, run_id=run.id)
-    return run_steps
-
-
-def wait_for_run(thread, run):
-    status = run.status
-    print(f"{status}")
-    while status in ["in_progress", "queued"]:
-        sleep(1)
-        run = update_run(thread, run)
-        status = run.status
-        print(f"{status}")
-    print(f"{status}")
-    return run
-
-
-def get_tool_calls(run):
-    tool_calls = run.required_action.submit_tool_outputs.tool_calls
-    return tool_calls
-
-
-def get_call_ids(tool_calls):
-    return [call.id for call in tool_calls]
-
-
-def submit_tool_outputs(thread, run, call_ids, outputs):
-    tool_outputs = [
-        {"tool_call_id": call_id, "output": output}
-        for call_id, output in zip(call_ids, outputs)
-    ]
-    run = client.beta.threads.runs.submit_tool_outputs(
-        thread_id=thread.id,
-        run_id=run.id,
-        tool_outputs=tool_outputs,
-    )
-    return run
-
-
-def setup_chat(assistant_name, instructions, tools, model):
-    assistant = create_assistant(
-        name=assistant_name, instructions=instructions, tools=tools, model=model
-    )
-    thread = create_thread()
-    return assistant, thread
-
-def get_tool_call_ids(run):
-    tool_calls = get_tool_calls(run)
-    call_ids = get_call_ids(tool_calls)
-    return call_ids
-
-if __name__ == "__main__":
-    name = "Web Browser Assistant"
-    instructions = "You are the operator of a web browser. Translate user requests into calls of the provided browser functions."
-    assistant = create_assistant(
-        name=name, instructions=instructions, tools=tools, model=MODEL
-    )
-    thread = create_thread()
-    message = "Visit google"
-    message_obj = add_message_to_thread(thread, message)
-    run = create_assistant_run(thread, assistant)
-    run = wait_for_run(thread, run)
-    tool_calls = get_tool_calls(run)
-    call_ids = get_call_ids(tool_calls)
-    outputs = ["Success"]
-    run = submit_tool_outputs(thread, run, call_ids, outputs)
-    run = wait_for_run(thread, run)
-    messages = list_messages(thread)
-    print(messages)
+def chat_completion_request(messages, functions=functions, model=GPT_MODEL):
+    try:
+        func_call: FunctionCall = client.chat.completions.create(
+            model=model,
+            temperature=0,
+            response_model=FunctionCall,
+            messages=messages,
+            functions=functions,
+            max_retries=2,
+        )
+        return func_call
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e}")
+        return e
--- main.py
+++ main.py
@@ -5,20 +5,7 @@ from playwright.sync_api import sync_playwright
 import json
 
 from drive_browser import WebDriver
-from head import (
-    create_assistant_run,
-    get_tool_call_ids,
-    visit_website,
-    execute_browser_action,
-    tools,
-    MODEL,
-    execute_function_call,
-    add_message_to_thread,
-    wait_for_run,
-    submit_tool_outputs,
-    list_messages,
-    setup_chat,
-)
+from head import chat_completion_request, execute_function_call
 
 
 load_dotenv()
@@ -33,32 +20,31 @@ def main():
             "content": "Navigate a web browser with the provided functions.",
         }
     )
-    name = "Web Browser Assistant"
-    instructions = "You are the operator of a web browser. Translate user requests into calls of the provided browser functions."
-    assistant, thread = setup_chat(name, instructions, tools, MODEL)
-
     with sync_playwright() as playwright:
         # driver = WebDriver(playwright)
         # driver.start_browser(headless=False)
-        user_input = input("Enter your message: ")
-        print(f"{user_input=}")
-        message = "Visit google"
-        message_obj = add_message_to_thread(thread, message)
-        run = create_assistant_run(thread, assistant)
-        run = wait_for_run(thread, run)
-        print("RUN STATUS: ", run.status)
-        call_ids = get_tool_call_ids(run)
-        outputs = ["Success"]
-        run = submit_tool_outputs(thread, run, call_ids, outputs)
-        run = wait_for_run(thread, run)
-        messages = list_messages(thread)
-        print(messages)
-        # if "function_call" in assistant_message:
-        #     function_call = assistant_message["function_call"]
-        #     execute_function_call(driver, function_call)  # Assuming this function is set up to handle the function call object
-        # else:
-        #     messages.append(assistant_message)
-        #     print("Assistant's response: ", assistant_message["content"])
+        while True:
+            user_input = input("Enter your message: ")
+            messages.append({"role": "user", "content": user_input})
+            func_call = chat_completion_request(messages)
+            print(f"{messages=}")
+            print(f"{func_call=}")
+
+            # driver = execute_function_call(driver, func_call)
+            messages = []
+            print(messages)
+            # if "function_call" in assistant_message:
+            #     function_call = assistant_message["function_call"]
+            #     execute_function_call(driver, function_call)  # Assuming this function is set up to handle the function call object
+            # else:
+            #     messages.append(assistant_message)
+            #     print("Assistant's response: ", assistant_message["content"])
+            # if "function_call" in assistant_message:
+            #     function_call = assistant_message["function_call"]
+            #     execute_function_call(driver, function_call)  # Assuming this function is set up to handle the function call object
+            # else:
+            #     messages.append(assistant_message)
+            #     print("Assistant's response: ", assistant_message["content"])
 
 
 if __name__ == "__main__":
